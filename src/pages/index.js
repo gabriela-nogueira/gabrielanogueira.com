5d2da7bf98a88524632bc214a3f2bf86d83c82a2
Gabriela Nogueira <gabriielanogueiira@gmail.com>
2020-07-30 17:53:38 -0300

    Changes

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,7 +2,7 @@ import React from "react"
 import { useStaticQuery, graphql } from 'gatsby';
 
 import Layout from "../components/Layout"
-import SEO from "../components/Seo"
+import SEO from "../components/seo"
 import ItemPost from "../components/ItemPost"
 
 
